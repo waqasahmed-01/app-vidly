12f90b39659ff7858f769fb4a3c94494563f58bd
waqasahmed-01 <mrahmedme1223@gmail.com>
2024-09-30 19:40:40 +0500

    New models for app

---
+++ models/genre.js
@@ -0,0 +1,45 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+//Schema,
+
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 20,
+  },
+});
+
+//Model,
+const Genre = mongoose.model('Genre', genreSchema);
+
+//Saving document in collection.
+async function saveDocument() {
+  const genre = new Genre({
+    name: 'Romance',
+  });
+  const result = await genre.save();
+  console.log(result);
+}
+
+//Retrieving documents from collection.
+async function displayDocuments() {
+  const result = await Genre.find().sort({ name: 1 }).select('name');
+  console.log(result);
+}
+
+//Validation function,
+function validateGenre(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(4).max(20).required(),
+  });
+  return schema.validate(genre);
+}
+
+module.exports.genreSchema = genreSchema;
+module.exports.Genre = Genre;
+module.exports.validate = validateGenre;
+module.exports.saveDoc = saveDocument;
+module.exports.displayDoc = displayDocuments;
