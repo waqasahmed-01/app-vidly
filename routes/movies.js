26b1f088a7ab8e76d13dd552da86bc2668a5bfa2
waqasahmed-01 <mrahmedme1223@gmail.com>
2024-10-04 22:24:20 +0500

    Updated movies routes

--- routes/movies.js
+++ routes/movies.js
@@ -8,6 +8,7 @@ router.get('/', async function (req, res) {
   res.status(200).json({ result: true, data: movies });
 });
 
+//Single id,
 router.get('/:id', async function (req, res) {
   const id = req.params.id;
   if (id.length !== 24)
@@ -17,6 +18,75 @@ router.get('/:id', async function (req, res) {
   if (!movie)
     return res.status(404).send('The movie with the given ID was not found.');
   res.status(200).json({ result: true, data: movie });
+});
+
+//Update,
+router.put('/:id', async function (req, res) {
+  const id = req.params.id;
+  if (id.length !== 24)
+    return res.status(400).send('Id lenght sould be 24 characters');
+
+  //Validations,
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send('Inavlid genre');
+
+  const movie = await Movie.findByIdAndUpdate(
+    id,
+    {
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
+    },
+    { new: true }
+  );
+
+  if (!movie)
+    return res.status(404).send('The movie with the given ID was not found.');
+
+  res.status(200).json({ result: true, data: movie });
+});
+
+//Creating,
+router.post('/', async function (req, res) {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send('Invalid genre');
+
+  let movie = new Movie({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+  movie = await movie.save();
+  res.status(200).json({ result: true, data: movie });
+});
+
+//Delete,
+router.delete('/:id', async function (req, res) {
+  const id = req.params.id;
+  if (id.length !== 24)
+    return res.status(400).send('Id lenght sould be 24 characters');
+
+  const movie = await Movie.findByIdAndDelete(id);
+  if (!movie)
+    return res.status(404).send('The movie with the given ID was not found.');
+
+  res
+    .status(200)
+    .json({ result: true, data: movie, message: 'Deleted successfully' });
 });
 
 module.exports = router;
