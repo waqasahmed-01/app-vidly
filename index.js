2618f026b201f025fa9a72b6a1b5de6e792a1b92
waqasahmed-01 <mrahmedme1223@gmail.com>
2024-10-08 10:00:56 +0500

    Updated index file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const customers = require('./routes/customers.js');
 const genres = require('./routes/genres.js');
+const rentals = require('./routes/rentals.js');
 const movies = require('./routes/movies.js');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -12,11 +13,15 @@ mongoose
   .then(() => console.log('Connected to database...'))
   .catch((error) => console.log('Something went wrong' + error));
 
+//Middleware.
 app.use(express.json());
 app.use(morgan('tiny'));
+
+//Handling routes.
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = global.process.env.PORT || 3000;
 
