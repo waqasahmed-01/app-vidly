16bed342ebe0b7c3c91b5b02480532d8ffe1e844
waqasahmed-01 <mrahmedme1223@gmail.com>
2024-10-30 12:18:47 +0500

    Implemented object-id validations

--- routes/rentals.js
+++ routes/rentals.js
@@ -2,6 +2,7 @@ const { Rental, validate } = require('../models/rental');
 const { Movie } = require('../models/movie');
 const { Customer } = require('../models/customer');
 const Fawn = require('fawn');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -15,11 +16,11 @@ router.get('/', async (req, res) => {
 
 //Single id,
 router.get('/:id', async (req, res) => {
-  const id = req.params.id;
-  if (id.length !== 24)
-    return res.status(400).send('Id length should be 24 characters');
+  const rentalId = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(rentalId))
+    return res.status(400).send('Rental objectid is invalid');
 
-  const rental = await Rental.findById(id);
+  const rental = await Rental.findById(rentalId);
   if (!rental)
     return res.status(404).send('The rental with the given ID was not found.');
 
@@ -31,11 +32,24 @@ router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const customer = await Customer.findById(req.body.customerId);
-  if (!customer) return res.status(400).send('Invalid customer.');
+  //Customer object-id validation,
+  const customerId = req.body.customerId;
+  if (!mongoose.Types.ObjectId.isValid(customerId))
+    return res.status(400).send('Invalid customer objectId.');
 
-  const movie = await Movie.findById(req.body.movieId);
-  if (!movie) return res.status(400).send('Invalid movie.');
+  //Find customer in database...
+  const customer = await Customer.findById(customerId);
+  if (!customer)
+    return res.status(400).send('Customer with given Id was not found.');
+
+  //Movie object-id validation,
+  const movieId = req.body.movieId;
+  if (!mongoose.Types.ObjectId.isValid(movieId))
+    return res.status(400).send('Invalid movie objectId.');
+
+  //Find movie in database...
+  const movie = await Movie.findById(movieId);
+  if (!movie) return res.status(400).send('Movie with given Id was not found.');
 
   if (movie.numberInStock === 0)
     return res.status(400).send('Movie not in stock.');
@@ -61,7 +75,9 @@ router.post('/', async (req, res) => {
 
     res.status(200).json({ result: true, data: rental });
   } catch (exp) {
-    res.status(500).json({ result: false, message: 'Something failed.' });
+    res
+      .status(500)
+      .json({ result: false, data: exp, message: 'Something failed.' });
   }
 });
 
