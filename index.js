6d5a540f6375e8c6df3e143eaa8ca5f3371a69dc
waqasahmed-01 <waqassoomroa@gmail.com>
2024-11-13 16:23:41 +0500

    Updated the index file

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const customers = require('./routes/customers.js');
 const movies = require('./routes/movies.js');
 const rentals = require('./routes/rentals.js');
 const users = require('./routes/users.js');
+const login = require('./routes/login.js');
+const config = require('config');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: JwtPrivakeKey is not defined.');
+  process.exit(1);
+}
 
 //Connecting to database.
 mongoose
@@ -23,6 +30,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/login', login);
 
 const port = global.process.env.PORT || 3000;
 
